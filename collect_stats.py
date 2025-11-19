112c690577cc1e64b5da81a6748b8da3a68cf417
Hongxu Xu <h445xu@uwaterloo.ca>
2025-11-19 03:07:40 +0000

    feat: update evaluation metrics and add best ID tracking in collect_stats

--- collect_stats.py
+++ collect_stats.py
@@ -23,6 +23,8 @@ def collect_stats(stats_file: str) -> MetricsList:
 def main():
     os.makedirs("final_stats", exist_ok=True)
 
+    best_id = dict()
+
     for path in glob.glob("eval_results/*.json"):
         metrics = collect_stats(path)
 
@@ -37,10 +39,15 @@ def main():
 
         for m in metrics.metrics:
             res.ttft += m.time_per_chunk[0]
-            res.tpot += m.total_time / m.n_generated_tokens
+            res.tpot += (m.total_time - m.time_per_chunk[0]) / m.n_generated_tokens
             res.latency += m.total_time
             res.n_generated_tokens += m.n_generated_tokens
             res.n_accepted_tokens += np.sum(m.n_accepted_draft_tokens_per_chunk).item()
+
+            if m.id in best_id:
+                best_id[m.id].append((res.id, m.total_time))
+            else:
+                best_id[m.id] = [(res.id, m.total_time)]
 
         res.ttft /= len(metrics.metrics)
         res.tpot /= len(metrics.metrics)
@@ -50,6 +57,14 @@ def main():
 
         with open(f"final_stats/{res.id}_final.json", "w") as f:
             f.write(res.model_dump_json(indent=2))
+
+    print("Best IDs per sample:")
+    for sample_id, id_list in best_id.items():
+        id_list = [x for x in id_list if "ngram" not in x[0]]
+        id_list.sort(key=lambda x: x[1])
+        if "nospec" in id_list[0][0]:
+            continue
+        print(f"{sample_id}: {id_list}")
 
 
 if __name__ == "__main__":
